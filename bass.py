35dfbe516d6cf147581b7ab249298b67c9a86926
dingxu6207 <32772701+dingxu6207@users.noreply.github.com>
2019-11-20 22:39:01 +0800

    数据检索

--- bass.py
+++ bass.py
@@ -9,24 +9,39 @@ import os
 from astropy.io import fits
 
 
-findobject = '\'53733\''
+findobject = '\'53753\''
 
-os.chdir('I:/BASS')
+os.chdir('I:/BASS/')
 path = os.getcwd()
 
 
-def findinfo(strfile):
+def findinfo(strfile): 
     phdulist = fits.open(strfile)
     objectname = phdulist[0].header['OBJECT']
     filtername = phdulist[0].header['FILTER']
     return objectname,filtername
 
-
+good = []
 for root, dirs, files in os.walk(path):
    for file in files:
        strfile = os.path.join(root, file)
-       objectname,filtername = findinfo(strfile)
-      # print(objectname)
+       try:
+           objectname,filtername = findinfo(strfile)
+       except:
+           continue
+       #print(objectname)
+       yuansu = (objectname,filtername)
+       good.append(yuansu)
        if (objectname == findobject):
            print(strfile)
            print(filtername)
+file = open('name.txt', 'w')
+myset = set(good)
+for item in myset:
+    #print(item,good.count(item))
+    try:
+        c=''.join(item)
+    except:
+        continue
+    file.write(c+'          '+str(good.count(item))+'\n')
+file.close()
